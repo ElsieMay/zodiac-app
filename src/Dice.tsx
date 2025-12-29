b79ce48817eb8d921c1165b2a0e64da781ccffc3
ElsieMay <elsiemaylawrie1@gmail.com>
2025-12-29 18:21:48 +0100

    Added more animating rings around centre dice.

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -15,19 +15,32 @@ function Dice() {
 		const mesh = new THREE.MeshNormalMaterial();
 		const geometry = new THREE.Mesh(new THREE.IcosahedronGeometry(), mesh);
 		scene.add(geometry);
-		geometry.scale.set(0.5, 0.5, 0.5);
-		// var geo = new THREE.RingGeometry(0.5, 1, 32, 2, 32);
-		var geo = new THREE.TorusGeometry(0.6, 0.03, 16, 42);
-		var ring = new THREE.Mesh(geo, mesh);
-		scene.add(ring);
-		renderer.setSize(600, 600);
+		geometry.scale.set(0.15, 0.15, 0.15);
+		var geoSmall = new THREE.TorusGeometry(0.6, 0.03, 16, 100);
+		var ringSmall = new THREE.Mesh(geoSmall, mesh);
+		scene.add(ringSmall);
+		ringSmall.scale.set(0.7, 0.7, 0.7);
+		var geoMedium = new THREE.TorusGeometry(0.9, 0.2, 16, 400);
+		var ringMedium = new THREE.Mesh(geoMedium, mesh);
+		scene.add(ringMedium);
+		var geoThin = new THREE.TorusGeometry(0.6, 0.004, 16, 100);
+		var ringThin = new THREE.Mesh(geoThin, mesh);
+		scene.add(ringThin);
+		var geoLarge = new THREE.TorusGeometry(1.2, 0.007, 16, 100);
+		var ringLarge = new THREE.Mesh(geoLarge, mesh);
+		scene.add(ringLarge);
+		renderer.setSize(700, 700);
 		containerRef.current.append(renderer.domElement);
 
 		function update() {
-			geometry.rotation.x += 2 / 100;
-			geometry.rotation.y += 2 / 100;
-			ring.rotation.x += 0.009;
-			ring.rotation.y += 0.009;
+			geometry.rotation.x += 2 / 200;
+			geometry.rotation.y += 2 / 200;
+			ringSmall.rotation.x += 0.003;
+			ringSmall.rotation.y += 0.003;
+			ringThin.rotation.x += 0.002;
+			ringThin.rotation.y += 0.002;
+			ringLarge.rotation.x += 0.004;
+			ringLarge.rotation.y += 0.004;
 		}
 
 		function render() {
